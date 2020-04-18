27882eaeea4f563e79ab13002ec1dc803bc61825
jorgeteruya <40158063+jorgeteruya@users.noreply.github.com>
2020-04-18 18:29:32 -0300

    Estruturado o client e server para o chat

--- APS Rede/src/Server.java
+++ APS Rede/src/Server.java
@@ -1,4 +1,33 @@
+import java.awt.im.InputContext;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
+	//Servidor tcp
+	
+	public static void main(String[] args) throws IOException{
+		
+		ServerSocket servidor = new ServerSocket();
+		
+		//recebe dados
+		Socket conexao = servidor.accept(); // espera a conexão ser estabelecida
+		InputStream is = conexao.getInputStream(); //tudo o que é enviado do cliente
+		System.out.println("Recebido do Cliente: " + (char) is.read());
+		
+		//Enviado
+		OutputStream enviaAoCliente = conexao.getOutputStream();
+		DataOutputStream dos = new DataOutputStream(enviaAoCliente);
+		dos.write(66);//66 = letra 'B' em ascii (write int)
+		dos.writeChars("teste"); //writeChars String 
+		
+		servidor.close();
+		conexao.close();
+		
+		
+	}
 
 }
